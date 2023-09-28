ffcad5e9359e1897a3a4e0eb29807e249641a839
Christoph Schneider <30388746+schch@users.noreply.github.com>
2023-09-28 15:47:46 +0000

    add validation for iban an error messages

--- src/app/fdm-registration/fdm-registration.component.ts
+++ src/app/fdm-registration/fdm-registration.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { StepperOrientation, MatStepperModule } from '@angular/material/stepper';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import {ValidatorService} from 'angular-iban';
 
 @Component({
 	selector: 'fdm-registration',
@@ -22,6 +23,7 @@ import { map } from 'rxjs/operators';
 		MatInputModule,
 		MatButtonModule,
 		AsyncPipe,
+		CommonModule
 	],
 	standalone: true,
 })
@@ -34,11 +36,33 @@ export class FdmRegistrationComponent {
 	bankFormGroup: FormGroup;
 
 	constructor(private _formBuilder: FormBuilder, breakpointObserver: BreakpointObserver) {
-		this.emailEntryFormGroup = this._formBuilder.group({ emailCtrl: ['', [Validators.required, Validators.email]] });
-		this.emailVerificationFormGroup = this._formBuilder.group({ emailVerifyCtrl: ['', [Validators.required]] });
-		this.contactFormGroup = this._formBuilder.group({ contactCtrl: [''] });
-		this.bankFormGroup = this._formBuilder.group({ bankCtrl: [''] });
+		// setup form controls
+		this.emailEntryFormGroup = this._formBuilder.group({ 
+			emailCtrl: ['', [Validators.required, Validators.email]]
+	 	});
+		this.emailVerificationFormGroup = this._formBuilder.group({ 
+			emailVerifyCtrl: ['', [Validators.required]]
+		});
 
+		this.contactFormGroup = this._formBuilder.group({ 
+			contactNameCtrl: ['', [Validators.required]],
+			contactAddressCtrl: [''],
+			contactPhoneCtrl: ['']
+		 });
+		this.bankFormGroup = this._formBuilder.group({
+			bankAmountCtrl: ['12', [Validators.required, Validators.min(12), Validators.pattern('[0-9]+(\.[0-9]{1,2})?')]],
+			bankNameCtrl: ['', [Validators.required]],
+			bankIBANCtrl: ['', [Validators.required, ValidatorService.validateIban]],
+			bankOwnerCtrl:  ['', [Validators.required]]
+		 });
+
+		 let name = this.contactFormGroup.get('contactNameCtrl') as FormControl;
+		 let owner = this.bankFormGroup.get('bankOwnerCtrl') as FormControl;
+		 name.valueChanges.subscribe((newValue: any) => {
+			owner.setValue(newValue);
+		 });
+
+		 // make form responsive
 		this.stepperOrientation = breakpointObserver
 			.observe('(min-width: 800px)')
 			.pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
