a325567882b9b6e865e8f166f8877e00408737d3
Christoph Schneider <30388746+schch@users.noreply.github.com>
2023-10-11 10:06:41 +0000

    add firebase config apikey restricted to id api

---
+++ src/app/FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyBIbzeN9i6A0xIqsRsXfuSGwcgJWYNxePc",
+  
+    authDomain: "fdm-anmeldung.firebaseapp.com",
+  
+    projectId: "fdm-anmeldung",
+  
+    //storageBucket: "fdm-anmeldung.appspot.com",
+  
+    //messagingSenderId: "382046571428",
+  
+    appId: "1:382046571428:web:b30a2b3d72522d84ec8f31"
+  
+  };
